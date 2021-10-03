ddf448b59d0f0d2e0c4f225d40d55b2ba0a39fe2
HEADEKBAT <57560182+HEADEKBAT@users.noreply.github.com>
2021-10-03 13:09:58 +0300

    add fullpage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 /** @format */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Fullpage from '@fullpage/react-fullpage';
+
+import Header from './components/Header';
+import FullPage from './components/Fullapge';
 
 function App() {
-	return <div className="App">Hello workd</div>;
+	return (
+		<div className="App">
+			{/* <Header /> */}
+			<FullPage />,
+		</div>
+	);
 }
 
 export default App;
